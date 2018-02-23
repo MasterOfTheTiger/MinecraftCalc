bbd2ec84817fdaa7058d2d4a56eecde12723d579
MasterOfTheTiger <ted.programmer@gmail.com>
2018-02-22 16:18:05 -0800

    Upload project

---
+++ scripts/default.js
@@ -0,0 +1,7 @@
+document.getElementById('error').style.display = 'none';
+
+function write(id, say) {
+  document.getElementById(id).innerHTML = say;
+}
+
+document.getElementById('wrapper').style.display = 'block';
